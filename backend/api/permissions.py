3712486c9e0cb7a61794096fdb623a5aab16c327
lukinMichail <120323852+lukinMichail@users.noreply.github.com>
2023-09-09 15:18:18 +0500

    Update permissions.py

--- backend/api/permissions.py
+++ backend/api/permissions.py
@@ -2,11 +2,7 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 
 class IsAuthenticatedOrReadOnly(BasePermission):
-    """
-    Чтения любым пользователям.
-    Создания, изменения, удаления только автору и администратору.
-    """
-
+   
     def has_permission(self, request, view):
         return request.method in SAFE_METHODS or request.user.is_authenticated
 
